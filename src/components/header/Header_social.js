158d521822b373a234eb6fe8aac1e7488564d900
Hp92663 <hirenkhut92663@gmail.com>
2023-11-02 21:33:23 +0530

    update Header_social.js

--- src/components/header/Header_social.js
+++ src/components/header/Header_social.js
@@ -5,9 +5,9 @@ import {BsLinkedin, BsInstagram,BsGithub} from 'react-icons/bs'
 const Header_social = () => {
   return (
     <div className='header_social'>
-        <a href='https://linkedin.com/' target='_blank'><BsLinkedin /></a>
-        <a href='https://github.com/' target='_blank'><BsInstagram /></a>
-        <a href='https://instagram.com/' target='_blank'><BsGithub /></a>
+        <a href='https://www.linkedin.com/in/hiren-khut/' target='_blank'><BsLinkedin /></a>
+        <a href='https://www.instagram.com/hiren_khut_92663/' target='_blank'><BsInstagram /></a>
+        <a href='https://github.com/Hp92663' target='_blank'><BsGithub /></a>
   {/* 
           <a href='https://www.linkedin.com/in/hiren-khut/' target='_blank'><BsLinkedin /></a>
           <a href='https://www.instagram.com/hiren_khut_92663/' target='_blank'><BsInstagram /></a>
