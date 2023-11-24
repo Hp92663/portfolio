4e14d4778199005e7953000634fb2fc9e2bf25f7
Hp92663 <hirenkhut92663@gmail.com>
2023-11-24 23:38:05 +0530

    complete 'nav' Section

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
 
+import { FiHome } from "react-icons/fi";
+import { FaRegUserCircle } from "react-icons/fa";
+import { MdLaptopChromebook } from "react-icons/md";
+import { RiServiceLine } from "react-icons/ri";
+import { RiCustomerService2Line } from "react-icons/ri";
+
+
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+        <a href='#'><FiHome /></a>
+        <a href='#about'><FaRegUserCircle /></a>
+        <a href='#experience'><MdLaptopChromebook /></a>
+        <a href='#service'><RiServiceLine /></a>
+        <a href='#contact'><RiCustomerService2Line /></a>
+    </nav>
   )
 }
 
