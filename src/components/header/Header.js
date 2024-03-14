68f441affc26524165446a2ddc7e62d06fd8f7f3
Hp92663 <hirenkhut92663@gmail.com>
2024-03-14 19:03:58 +0530

    update 5 files

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,36 +1,35 @@
-import React from 'react'
+import React from "react";
 
-import "./header.css"
+import "./header.css";
 
-import CTA from './CTA'
-import Header_social from './Header_social';
+import CTA from "./CTA";
+import Header_social from "./Header_social";
 
-import ME from '../../assets/me.png';
+import ME from "../../assets/me.png";
 
 const Header = () => {
   return (
     <header>
       <div className="container header_container">
         <h5>Hello I'm</h5>
-        <h1>Hiren Khut</h1>
-        <h5 className='text-light'>Web Developer</h5>
+        <h1>Darshil Ghatala</h1>
+        <h5 className="text-light">Web Developer</h5>
         <CTA />
-       <Header_social />
+        <Header_social />
 
-       {/* Add Image */}
-      <div className='me'>
-        <img src={ME} alt='me'/>
+        {/* Add Image */}
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        {/* scroll */}
+
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
       </div>
-
-      {/* scroll */}
-
-     <a href='#contact' className='scroll_down'>Scroll Down</a>
-
-      </div>
-
     </header>
-    )
+  );
+};
 
-}
-
-export default Header
+export default Header;
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -7,13 +7,17 @@ import { MdLaptopChromebook } from "react-icons/md";
 import { RiServiceLine } from "react-icons/ri";
 import { RiCustomerService2Line } from "react-icons/ri";
 
+import { useState } from 'react';
 
 const Nav = () => {
+
+  const [activeNav, setActiveNav] = useState('#')
+
   return (
     <nav>
-        <a href='#'><FiHome /></a>
-        <a href='#about'><FaRegUserCircle /></a>
-        <a href='#experience'><MdLaptopChromebook /></a>
+        <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ?  'active': ''}><FiHome /></a>
+        <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaRegUserCircle /></a>
+        <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdLaptopChromebook /></a>
         <a href='#service'><RiServiceLine /></a>
         <a href='#contact'><RiCustomerService2Line /></a>
     </nav>
